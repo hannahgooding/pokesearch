03d27f52737831d92504a7e6edee17913818887a
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-03-14 22:24:25 +0000

    Add bg colours to type output

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const form = document.querySelector("form");
 const output = document.querySelector(".output");
 const outputPokemonName = document.createElement("p");
 const outputPokemonImage = document.createElement("img");
-const outputPokemonType = document.createElement("p");
+const outputPokemonTypeOne = document.createElement("p");
+const outputPokemonTypeTwo = document.createElement("p");
 
 form.addEventListener("submit", e => {
     e.preventDefault();
@@ -20,21 +21,145 @@ form.addEventListener("submit", e => {
             outputPokemonName.textContent = res.name[0].toUpperCase() + res.name.slice(1);
             // capitalise pokemon name
             outputPokemonImage.src = res.sprites.front_default;
+            output.appendChild(outputPokemonName);
+            output.appendChild(outputPokemonImage);
 
             const pokemonTypes = res.types;
             let pokemonTypesArray = [];
             Object.keys(pokemonTypes).forEach(key => {
                 pokemonTypesArray.push(pokemonTypes[key].type.name);
-            });
-            outputPokemonType.textContent = pokemonTypesArray.join(" ").toUpperCase();
+            });            
 
-            output.appendChild(outputPokemonName);
-            output.appendChild(outputPokemonImage);
-            output.appendChild(outputPokemonType);
+            if (pokemonTypesArray.length === 1) {
+                outputPokemonTypeOne.textContent = pokemonTypesArray[0];
+                output.appendChild(outputPokemonTypeOne);
+            } else {
+                outputPokemonTypeOne.textContent = pokemonTypesArray[0];
+                outputPokemonTypeTwo.textContent = pokemonTypesArray[1];
+                output.appendChild(outputPokemonTypeOne);
+                output.appendChild(outputPokemonTypeTwo);
+            }
 
-            outputPokemonName.setAttribute("class", "output__pokemon-name");
-            outputPokemonImage.setAttribute("class", "output__pokemon-image");
-            outputPokemonType.setAttribute("class", "output__pokemon-type");
+            switch (outputPokemonTypeOne.textContent) {
+                case "normal":
+                    outputPokemonTypeOne.setAttribute("class", "output__pokemon-type output__pokemon-type--normal");
+                    break;
+                case "fighting":
+                    outputPokemonTypeOne.setAttribute("class", "output__pokemon-type output__pokemon-type--fighting");
+                    break;
+                case "flying":
+                    outputPokemonTypeOne.setAttribute("class", "output__pokemon-type output__pokemon-type--flying");
+                    break;
+                case "poison":
+                    outputPokemonTypeOne.setAttribute("class", "output__pokemon-type output__pokemon-type--poison");
+                    break;
+                case "ground":
+                    outputPokemonTypeOne.setAttribute("class", "output__pokemon-type output__pokemon-type--ground");
+                    break;
+                case "rock":
+                    outputPokemonTypeOne.setAttribute("class", "output__pokemon-type output__pokemon-type--rock");
+                    break;
+                case "bug":
+                    outputPokemonTypeOne.setAttribute("class", "output__pokemon-type output__pokemon-type--bug");
+                    break;
+                case "ghost":
+                    outputPokemonTypeOne.setAttribute("class", "output__pokemon-type output__pokemon-type--ghost");
+                    break;
+                case "steel":
+                    outputPokemonTypeOne.setAttribute("class", "output__pokemon-type output__pokemon-type--steel");
+                    break;
+                case "fire":
+                    outputPokemonTypeOne.setAttribute("class", "output__pokemon-type output__pokemon-type--fire");
+                    break;
+                case "water":
+                    outputPokemonTypeOne.setAttribute("class", "output__pokemon-type output__pokemon-type--water");
+                    break;
+                case "grass":
+                    outputPokemonTypeOne.setAttribute("class", "output__pokemon-type output__pokemon-type--grass");
+                    break;
+                case "electric":
+                    outputPokemonTypeOne.setAttribute("class", "output__pokemon-type output__pokemon-type--electric");
+                    break;
+                case "psychic":
+                    outputPokemonTypeOne.setAttribute("class", "output__pokemon-type output__pokemon-type--pyschic");
+                    break;
+                case "water":
+                    outputPokemonTypeOne.setAttribute("class", "output__pokemon-type output__pokemon-type--water");
+                    break;
+                case "ice":
+                    outputPokemonTypeOne.setAttribute("class", "output__pokemon-type output__pokemon-type--ice");
+                    break;  
+                case "dragon":
+                    outputPokemonTypeOne.setAttribute("class", "output__pokemon-type output__pokemon-type--dragon");
+                    break;  
+                case "dark":
+                    outputPokemonTypeOne.setAttribute("class", "output__pokemon-type output__pokemon-type--dark");
+                    break;
+                case "fairy":
+                    outputPokemonTypeOne.setAttribute("class", "output__pokemon-type output__pokemon-type--fairy");
+                    break;       
+            }
+
+            switch (outputPokemonTypeTwo.textContent) {
+                case "normal":
+                outputPokemonTypeTwo.setAttribute("class", "output__pokemon-type output__pokemon-type--normal");
+                break;
+                case "fighting":
+                outputPokemonTypeTwo.setAttribute("class", "output__pokemon-type output__pokemon-type--fighting");
+                break;
+                case "flying":
+                outputPokemonTypeTwo.setAttribute("class", "output__pokemon-type output__pokemon-type--flying");
+                break;
+                case "poison":
+                outputPokemonTypeTwo.setAttribute("class", "output__pokemon-type output__pokemon-type--poison");
+                break;
+                case "ground":
+                outputPokemonTypeTwo.setAttribute("class", "output__pokemon-type output__pokemon-type--ground");
+                break;
+                case "rock":
+                outputPokemonTypeTwo.setAttribute("class", "output__pokemon-type output__pokemon-type--rock");
+                break;
+                case "bug":
+                outputPokemonTypeTwo.setAttribute("class", "output__pokemon-type output__pokemon-type--bug");
+                break;
+                case "ghost":
+                outputPokemonTypeTwo.setAttribute("class", "output__pokemon-type output__pokemon-type--ghost");
+                break;
+                case "steel":
+                outputPokemonTypeTwo.setAttribute("class", "output__pokemon-type output__pokemon-type--steel");
+                break;
+                case "fire":
+                outputPokemonTypeTwo.setAttribute("class", "output__pokemon-type output__pokemon-type--fire");
+                break;
+                case "water":
+                outputPokemonTypeTwo.setAttribute("class", "output__pokemon-type output__pokemon-type--water");
+                break;
+                case "grass":
+                outputPokemonTypeTwo.setAttribute("class", "output__pokemon-type output__pokemon-type--grass");
+                break;
+                case "electric":
+                outputPokemonTypeTwo.setAttribute("class", "output__pokemon-type output__pokemon-type--electric");
+                break;
+                case "psychic":
+                    outputPokemonTypeTwo.setAttribute("class", "output__pokemon-type output__pokemon-type--pyschic");
+                    break;
+                case "water":
+                outputPokemonTypeTwo.setAttribute("class", "output__pokemon-type output__pokemon-type--water");
+                break;
+                case "ice":
+                outputPokemonTypeTwo.setAttribute("class", "output__pokemon-type output__pokemon-type--ice");
+                break; 
+                case "dragon":
+                outputPokemonTypeTwo.setAttribute("class", "output__pokemon-type output__pokemon-type--dragon");
+                break; 
+                case "dark":
+                outputPokemonTypeTwo.setAttribute("class", "output__pokemon-type output__pokemon-type--dark");
+                break;
+                case "fairy":
+                outputPokemonTypeTwo.setAttribute("class", "output__pokemon-type output__pokemon-type--fairy");
+                break; 
+                }
+
         })
         .catch(error => {
             console.log(error);
